39ebc6270cfc00800ec13286067c295abc5d1653
Dani4772 <adnanameen4772@gmail.com>
2022-10-26 10:52:13 +0500

    ui designs and api connect

---
+++ lib/models/login_page_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_page_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LogInModel _$LogInModelFromJson(Map<String, dynamic> json) => LogInModel(
+      email: json['email'] as String?,
+      password: json['password'] as String?,
+    );
+
+Map<String, dynamic> _$LogInModelToJson(LogInModel instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'password': instance.password,
+    };
